cae09c87cb62130b792580468ba90b475f4971b8
Mansai Conner <mansaiconner@gmail.com>
2026-01-26 15:08:05 -0800

    wrote leapYears function.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 == 0 || year % 4 != 0) {
+    if (year % 400 == 0) return true;
+    return false;
+  } else if (year % 4 == 0) return true;
 };
 
 // Do not edit below this line
