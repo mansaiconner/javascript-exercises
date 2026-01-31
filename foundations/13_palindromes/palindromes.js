a54945fb2c1ddb8cecc1a535a8b38a931fd39a6b
Mansai Conner <mansaiconner@gmail.com>
2026-01-30 16:42:08 -0800

    Did exercise.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
+  cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanum.includes(char))
+    .join("");
+  reversedString = cleanedString.split("").reverse().join("");
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
