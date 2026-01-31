92ba19d4e6d87265a373cb85acd12e7d9418da5b
Mansai Conner <mansaiconner@gmail.com>
2026-01-30 16:54:52 -0800

    Did exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (+num < 0) {
+    return "OOPS";
+  }
+  if (+num === 0) {
+    return 0;
+  }
+  if (+num === 1 || +num === 2) {
+    return 1;
+  }
+  let prev2 = 1;
+  let prev = 1;
+  next = 2;
 
+  for (let i = 3; i <= +num; i++) {
+    next = prev2 + prev;
+    prev2 = prev;
+    prev = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
