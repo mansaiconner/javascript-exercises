c92f32588eca0b6cef10baf2fa8050c461d1dee2
Mansai Conner <mansaiconner@gmail.com>
2026-01-30 17:25:27 -0800

    Did exercise.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const ordered = people.sort((a, b) => {
+    currentYear = new Date().getFullYear();
+    firstPerson = 0;
+    nextPerson = 0;
 
+    if (a.yearOfDeath === undefined) {
+      firstPerson = currentYear - a.yearOfBirth;
+    }
+    if (b.yearOfDeath === undefined) {
+      nextPerson = currentYear - b.yearOfBirth;
+    }
+    if (a.yearOfDeath !== undefined) {
+      firstPerson = a.yearOfDeath - a.yearOfBirth;
+    }
+    if (b.yearOfDeath !== undefined) {
+      nextPerson = b.yearOfDeath - b.yearOfBirth;
+    }
+    return nextPerson - firstPerson;
+  });
+  return ordered[0];
 };
 
 // Do not edit below this line
